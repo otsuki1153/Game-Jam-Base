4123ea76637b65dab7bee59af78e6eb02911e7ed
FireFox1312 <cadupurkott@gmail.com>
2026-01-31 15:24:05 -0300

    Enemy heatlh and attack
    Implemented mechanicals of attack and health for enemy character

--- treino-de-2.5-3d/Characters Scenes/Enemy.gd
+++ treino-de-2.5-3d/Characters Scenes/Enemy.gd
@@ -1,31 +1,33 @@
 extends CharacterBody3D
 
+#---------------------------------------  atributos ---------------------------------------
+
 @export var speed: float 
 @export var gravity: float = 10
-@export var friction : float 
-@export var acceleration : float = 50
+@export var friction: float 
+@export var acceleration: float = 10
+@export var max_health: float
+@export var base_attack: float
 
+var current_health : float
+var can_attack: bool = false
 
-##----------------------------------  State Machine  ----------------------------------
 @export var player: CharacterBody3D 
 
-@onready var timer_to_stay: Timer = $timer_to_stay
-@onready var timer_warking: Timer = $timer_warking
-@onready var timer_turnig: Timer = $timer_turnig
 @onready var frente: RayCast3D = $RayCast3D
-@onready var vision: Area3D = $Vision
+@onready var attack_range: Area3D = $AttackRange
 
-var direction : Vector3 = Vector3.ZERO
+var direction: Vector3 = Vector3.ZERO
 
-enum states {stay, patrol, turn, hunt}
-var axis_turn: bool = false
-var axis: bool = false
-var actual_state = states.patrol
+func _ready() -> void:
+	add_to_group("enemies")
+	can_attack = false
+	current_health = max_health
 
 func _physics_process(delta: float) -> void:
 	gravidade(delta)
 	movement(delta)
-	#state_machine(delta)
+	attack(base_attack)
 	move_and_slide()
 
 func gravidade (delta):
@@ -34,105 +36,39 @@ func gravidade (delta):
 	else:
 		velocity.y = 0
 
-#func state_machine (delta):
-	#match actual_state:
-		#states.stay:
-			#velocity.x = 0.0
-			#velocity.z = 0.0
-		#states.patrol:
-			#if timer_to_stay.time_left == 0.0: 
-				#timer_to_stay.start(25)
-			#if axis_turn:
-				#if axis:
-					#velocity.x = move_toward(velocity.x, direction.x * speed, acceleration * delta) 
-					#frente.rotation.y = deg_to_rad(0.0)
-					#vision.rotation.y = deg_to_rad(0.0)
-					#if timer_warking.is_stopped():
-						#timer_warking.start(5.0)
-				#else:
-					#velocity.x = move_toward(velocity.x, -(direction.x * speed), acceleration * delta )
-					#frente.rotation.y = deg_to_rad(180.0)
-					#vision.rotation.y = deg_to_rad(180.0)
-					#if timer_warking.is_stopped():
-						#timer_warking.start(5.0)
-			#else:
-				#if axis:
-					#velocity.z = move_toward(velocity.z, direction.z * speed, acceleration * delta) 
-					#frente.rotation.y = deg_to_rad(90.0)
-					#vision.rotation.y = deg_to_rad(90.0)
-					#if timer_warking.is_stopped():
-						#timer_warking.start(5.0)
-				#else:
-					#velocity.z = move_toward(velocity.z, -(direction.z * speed), acceleration * delta )
-					#frente.rotation.y = deg_to_rad(270.0)
-					#vision.rotation.y = deg_to_rad(270.0)
-					#if timer_warking.is_stopped():
-						#timer_warking.start(5.0)
-			#if frente.is_colliding():
-				#timer_warking.stop()
-				#actual_state = states.turn
-		#states.turn:
-			#velocity.x = move_toward(velocity.x, 0.0,friction * delta) 
-			#velocity.z = move_toward(velocity.z, 0.0,friction *  delta) 
-			#if timer_turnig.time_left == 0.0:
-				#timer_turnig.start(3.0)
-		#states.hunt:
-			#timer_warking.stop()
-			#timer_turnig.stop()
-			#timer_to_stay.stop()
-			#var target : Vector3 = (player.global_position -  self.global_position)
-			#target.y = 0.0
-			#target = target.normalized()
-			#var angle_target = atan2(target.x , target.z)
-			#
-			#velocity.x = move_toward(velocity.x, (target.x * speed), acceleration * delta)
-			#velocity.z = move_toward(velocity.z, (target.z * speed), acceleration * delta)
-			#self.rotation.y = lerp_angle(rotation.y, angle_target,0.5 * delta)
-			
-
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		print("player entrou")
-		actual_state = states.hunt
-	
-
+		can_attack = true
+		
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		print("player saiu")
-		actual_state = states.patrol
-		timer_turnig.start(3.0)
-	
+		can_attack = false
 
+func attack(amount):
+	if frente.is_colliding() and can_attack:
+		print("toma na boca!!!")
+		player.current_health -= amount
 
-func _on_timer_turnig_timeout() -> void:
-	axis_turn = !axis_turn
-	actual_state = states.patrol
-
-
-func _on_timer_warking_timeout() -> void:
-	axis = !axis
-
-
-func _on_timer_to_stay_timeout() -> void:
-	actual_state = states.stay
+func health_update(amount: float):
+	current_health -= amount
+	print(current_health)
 
 func movement(delta):
 	if not player:
 		return
 	
 	else:
-		var target : Vector3 = (player.global_position -  self.global_position).normalized()
-		var angle_target = atan2(target.x , target.z)
-		
-		target.y = 0.0
-		velocity.x = move_toward(velocity.x, (target.x * speed), acceleration * delta)
-		velocity.z = move_toward(velocity.z, (target.z * speed), acceleration * delta)
-		self.rotation.y = lerp_angle(rotation.y, angle_target,0.5 * delta)
+		persecution(delta)
 
-
-
-
-func persecution():
-	return
+func persecution(delta):
+	var target : Vector3 = (player.global_position -  self.global_position).normalized()
+	var angle_target = atan2(target.x , target.z)
+	
+	target.y = 0.0
+	velocity.x = move_toward(velocity.x, (target.x * speed), acceleration * delta)
+	velocity.z = move_toward(velocity.z, (target.z * speed), acceleration * delta)
+	self.rotation.y = lerp_angle(rotation.y, angle_target,5.0 * delta)
