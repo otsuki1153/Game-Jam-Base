2be728e5842b9702e860f57aa323fd87f904dba9
otsuki1153 <107266141+otsuki1153@users.noreply.github.com>
2026-01-30 22:14:57 -0300

    fix(cam)
    arrumado a câmera que anteriormente era livre foi para câmera fixa.

--- treino-de-2.5-3d/Characters Scenes/player.gd
+++ treino-de-2.5-3d/Characters Scenes/player.gd
@@ -6,13 +6,16 @@ extends CharacterBody3D
 @export var friction: float  = 18.0
 @export var Gravidade: float = 24.0
 
-@onready var cam_pivot: Node3D = $Cam_pivot
-@onready var camera_3d: Camera3D = $Cam_pivot/Camera3D
 
-@export var mouse_sensi: float = 0.005
-@export var cameraRotation: Vector2 = Vector2.ZERO
+var paused: bool = false
 
-var mouse_delta := Vector2.ZERO
+#@onready var cam_pivot: Node3D = $Cam_pivot
+#@onready var camera_3d: Camera3D = $Cam_pivot/Camera3D
+
+#@export var mouse_sensi: float = 0.005
+#@export var cameraRotation: Vector2 = Vector2.ZERO
+
+#var mouse_delta := Vector2.ZERO
 
 func _ready() -> void:
 	up_direction = Vector3.UP
@@ -22,31 +25,35 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
+		paused = !paused
+	
+	if paused:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-	if Input.is_action_just_pressed("Left_mouse"):
+	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#if Input.is_action_just_pressed("Left_mouse"):
+		#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		mouse_delta = event.relative
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		#mouse_delta = event.relative
 
-
-func cameraMovement(delta):
-	rotate_y(-mouse_delta.x * mouse_sensi)
-	
-	
-	cam_pivot.rotation.x += mouse_delta.y * mouse_sensi
-	cam_pivot.rotation.x = clamp(cam_pivot.rotation.x, deg_to_rad(-33),  deg_to_rad(33))
-	
-	mouse_delta = Vector2.ZERO
-
+#
+#func cameraMovement(delta):
+	#rotate_y(-mouse_delta.x * mouse_sensi)
+	#
+	#
+	#cam_pivot.rotation.x += mouse_delta.y * mouse_sensi
+	#cam_pivot.rotation.x = clamp(cam_pivot.rotation.x, deg_to_rad(-33),  deg_to_rad(33))
+	#
+	#mouse_delta = Vector2.ZERO
+#
 
 
 func _physics_process(delta: float) -> void:
-	cameraMovement(delta)
+	#cameraMovement(delta)
 	gravity(delta)
 	movement(delta)
 	move_and_slide()
@@ -61,19 +68,23 @@ func gravity(delta: float) -> void:
 
 func movement(delta: float) -> void:
 	var inputDir := Input.get_vector("A", "D", "W", "S")
-	
+	var direction: Vector3 = Vector3(inputDir.x, 0, inputDir.y)
 	 
-	var forward := camera_3d.global_basis.z
-	var right := camera_3d.global_basis.x
-	var direction: Vector3 = forward * inputDir.y + right * inputDir.x
-	direction.y = 0.0
+	#var forward := camera_3d.global_basis.z
+	#var right := camera_3d.global_basis.x
+	#direction.y = 0.0
 	
 	
-	
-	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_FORCE
-	
-	if direction:
-		velocity = velocity.move_toward(direction * SPEED, acceleration * delta)
+	if !paused:
+		if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_FORCE
+		
+		if inputDir != Vector2.ZERO:
+			velocity.x = move_toward(velocity.x, direction.x * SPEED, acceleration * delta)
+			velocity.z = move_toward(velocity.z, direction.z * SPEED, acceleration * delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, friction * delta)
+			velocity.z = move_toward(velocity.z, 0, friction * delta)
 	else:
-		velocity = velocity.move_toward(Vector3.ZERO, friction * delta)
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+		velocity.z = move_toward(velocity.z, 0, friction * delta)
